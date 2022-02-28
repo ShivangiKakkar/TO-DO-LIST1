24b5654d10d5220b424ec13dfd42c45ccfdf7a1f
Shivangi Kakkar <skakkar@skakkar-l5jgh5.lan>
2022-02-28 03:43:23 -0500

    added calendar component

---
+++ client/src/components/Calendar.vue
@@ -0,0 +1,187 @@
+<script setup lang="ts">
+</script>
+<template>
+    <!-- FEB -->
+  <div class="feb">
+    <div class="section container columns is-one-quarter">
+                    <div class="card">
+                        <header class="card-header">
+                          <p class="card-header-title">
+                            February
+                          </p>
+                          <button class="card-header-icon" aria-label="more options">
+                            <span class="icon">
+                              <i class="fas fa-angle-down" aria-hidden="true"></i>
+                            </span>
+                          </button>
+                        </header>
+                        <div class="card-content">
+                          <div class="content">
+                            <table class="table">
+                              <tr>
+                                <th>M</th>
+                                <th>T</th>
+                                <th>W</th>
+                                <th>T</th>
+                                <th>F</th>
+                                <th>S</th>
+                                <th>S</th>
+                              </tr>
+                              <tr>
+                                <td>-</td>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                              </tr>
+                              <tr>
+                                <td>7</td>
+                                <td>8</td>
+                                <td>9</td>
+                                <td>10</td>
+                                <td>11</td>
+                                <td>12</td>
+                                <td>13</td>
+                              </tr>
+                              <tr>
+                                <td>14</td>
+                                <td>15</td>
+                                <td>16</td>
+                                <td>17</td>
+                                <td>18</td>
+                                <td>19</td>
+                                <td>20</td>
+                              </tr>
+                              <tr>
+                                <td>21</td>
+                                <td>22</td>
+                                <td>23</td>
+                                <td>24</td>
+                                <td>25</td>
+                                <td>26</td>
+                                <td>27</td>
+                              </tr>
+                              <tr>
+                                <td>28</td>
+                              </tr>
+                            </table>
+                            <time class= "time" datetime="2016-1-1">11:09 PM - 16 Feb 2022</time>
+                          </div>
+                        </div>
+                        <footer class="card-footer">
+                          <a href="#" class="card-footer-item">Save</a>
+                          <a href="#" class="card-footer-item">Edit</a>
+                          <a href="#" class="card-footer-item">Delete</a>
+                        </footer>
+                      </div>
+                </div>
+  </div>
+
+                <!-- MARCH -->
+  <div class="mar">
+        <div class="section container columns is-one-quarter">
+                              <div class="card">
+                                  <header class="card-header">
+                                    <p class="card-header-title">
+                                      March
+                                    </p>
+                                    <button class="card-header-icon" aria-label="more options">
+                                      <span class="icon">
+                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                      </span>
+                                    </button>
+                                  </header>
+                                  <div class="card-content">
+                                    <div class="content">
+                                      <table class="table">
+                                        <tr>
+                                          <th>M</th>
+                                          <th>T</th>
+                                          <th>W</th>
+                                          <th>T</th>
+                                          <th>F</th>
+                                          <th>S</th>
+                                          <th>S</th>
+                                        </tr>
+                                        <tr>
+                                          <td>-</td>
+                                          <td>1</td>
+                                          <td>2</td>
+                                          <td>3</td>
+                                          <td>4</td>
+                                          <td>5</td>
+                                          <td>6</td>
+                                        </tr>
+                                        <tr>
+                                          <td>7</td>
+                                          <td>8</td>
+                                          <td>9</td>
+                                          <td>10</td>
+                                          <td>11</td>
+                                          <td>12</td>
+                                          <td>13</td>
+                                        </tr>
+                                        <tr>
+                                          <td>14</td>
+                                          <td>15</td>
+                                          <td>16</td>
+                                          <td>17</td>
+                                          <td>18</td>
+                                          <td>19</td>
+                                          <td>20</td>
+                                        </tr>
+                                        <tr>
+                                          <td>21</td>
+                                          <td>22</td>
+                                          <td>23</td>
+                                          <td>24</td>
+                                          <td>25</td>
+                                          <td>26</td>
+                                          <td>27</td>
+                                        </tr>
+                                        <tr>
+                                          <td>28</td>
+                                          <td>29</td>
+                                          <td>30</td>
+                                          <td>31</td>
+                                        </tr>
+                                      </table>
+                                      <time class= "time" datetime="2016-1-1">11:09 PM - 16 Feb 2022</time>
+                                    </div>
+                                  </div>
+                                  <footer class="card-footer">
+                                    <a href="#" class="card-footer-item">Save</a>
+                                    <a href="#" class="card-footer-item">Edit</a>
+                                    <a href="#" class="card-footer-item">Delete</a>
+                                  </footer>
+                                </div>
+                          </div>
+  </div>
+  
+</template>
+
+<style>
+
+    .is-done{
+          text-decoration: line-through;
+      }
+    .calendar-icon {
+        margin-left: 10px;
+        font-weight: bold;
+      }
+    .card-content {
+        padding: 0.5rem;
+      }
+    .table {
+        background-color: rgb(232, 245, 250);
+      }
+    table {
+        border: 1px solid #dddddd;
+      }
+    .time {
+        font-weight: bold;
+      }
+</style>
+
