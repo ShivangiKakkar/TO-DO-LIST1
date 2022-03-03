489965ff70b6e0f31d9bc8d0bae09c48c3a4a640
Shivangi Kakkar <skakkar@client154-106.wireless.newpaltz.edu>
2022-03-03 16:13:54 -0500

    cosmetic changes

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import "bulma";
+import "@fortawesome/fontawesome-free/css/all.css";
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
