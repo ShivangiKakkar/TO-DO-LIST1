792c0fb22c1d57a052c1f5c1cf067b54db144c73
Shivangi Kakkar <skakkar@skakkar-l5jgh5.lan>
2022-05-24 17:19:03 -0400

    cosmetic changes

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia';
 import "bulma";
 import "@fortawesome/fontawesome-free/css/all.css";
 import App from './App.vue';
-import Oruga from "@oruga-ui/oruga-next";
+// import Oruga from "@oruga-ui/oruga-next";
 import "@oruga-ui/oruga-next/dist/oruga.css";
 import router from './router'
 
 createApp(App)
     .use(router)
     .use(createPinia())
-    .use(Oruga)
+    // .use(Oruga)
     .mount('#app');
